b1eaa74328ef531dd40f3707d23a0d3ce84bdef1
OmiAtTexas <opm281706@gmail.com>
2026-06-05 18:41:12 -0500

    Fix root redirect

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,5 @@
 import { redirect } from 'next/navigation';
-import { auth } from '@clerk/nextjs/server';
 
-export default async function RootPage() {
-  const { userId } = await auth();
-  if (userId) redirect('/home');
-  redirect('/auth/sign-in');
+export default function RootPage() {
+  redirect('/home');
 }
