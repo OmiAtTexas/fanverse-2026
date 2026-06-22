4dd304c59dd6b4c4b8b58dcd663eabbe2a14c6b1
OmiAtTexas <opm281706@gmail.com>
2026-06-22 10:32:10 -0500

    Remove committed dist folder, add to gitignore

--- apps/api/dist/matches/matches.controller.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class MatchesController {
-    private fetchESPN;
-    getLive(): Promise<any>;
-    findAll(date?: string): Promise<any>;
-    getSchedule(dates?: string): Promise<any>;
-}
